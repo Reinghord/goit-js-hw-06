39779903e3d203d84c26f42ec4fefe0e46198a9b
Reinghord <105374551+Reinghord@users.noreply.github.com>
2022-06-29 23:34:42 +0300

    bugfix

--- js/task-06.js
+++ js/task-06.js
@@ -5,6 +5,9 @@ const { input } = {
 input.addEventListener("blur", () => {
   if (input.value.length === Number(input.getAttribute("data-length"))) {
     input.classList.add("valid");
+    input.classList.remove("invalid");
+  } else {
+    input.classList.add("invalid");
+    input.classList.remove("valid");
   }
-  input.classList.add("invalid");
 });
